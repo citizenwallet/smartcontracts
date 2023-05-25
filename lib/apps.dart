971d3ba2e5be6c17eb200305c8adf45f2700750d
kev-techi <kev-techi@users.noreply.github.com>
2023-05-25 22:30:44 +0000

    [action] compile contracts

--- lib/apps.dart
+++ lib/apps.dart
@@ -3,4 +3,8 @@
 /// This file is auto-generated, edit scripts/compile.sh to modify
 library;
 
+export 'contracts/apps/GratitudeTokenFactory.g.dart';
+export 'contracts/apps/GratitudeToken.g.dart';
+export 'contracts/apps/ProfileFactory.g.dart';
+export 'contracts/apps/Profile.g.dart';
 export 'contracts/apps/RegensUniteTokens.g.dart';
