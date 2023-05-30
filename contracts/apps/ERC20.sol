573f775c4e353746fb4c33b5f201d56d23bc5ca8
kevin <kevin@twire.se>
2023-05-30 14:04:59 +0200

    [added] erc20

---
+++ contracts/apps/ERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+
+contract ERC20 is ERC20Upgradeable, ERC20BurnableUpgradeable {}
