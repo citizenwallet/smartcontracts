6df1f11bc97d4874ed87e7ef9f73d81da7d57dea
kev-techi <kevin@techi.be>
2023-07-24 17:53:28 +0200

    [added] username and getting profile by username

--- contracts/apps/Profile.sol
+++ contracts/apps/Profile.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
@@ -12,6 +12,16 @@ contract Profile is
     OwnableUpgradeable,
     UUPSUpgradeable
 {
+    bytes32 constant NULL = "";
+
+    // only a single username per address
+    // allows us to check which username was already reserved by a profile
+    mapping(address profile => bytes32 username) public usernames;
+
+    // only a single username per profile
+    // allows us to easily fetch a profile from a username
+    mapping(bytes32 username => address profile) public profiles;
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -23,21 +33,46 @@ contract Profile is
         __UUPSUpgradeable_init();
     }
 
-    function set(address profile, string memory _uri) public returns (uint256) {
+    function set(
+        address profile,
+        bytes32 _username,
+        string memory _uri
+    ) public returns (uint256) {
         require(profile == msg.sender, "Only the profile owner can set it.");
+
+        bytes32 username = usernames[profile];
+
+        require(
+            (username == NULL) || // username is not set
+                (username != NULL && profiles[username] == profile), // username is set but belongs to the profile
+            "This username is already taken."
+        );
 
         uint256 newProfileId = _fromAddressToId(profile);
         if (_exists(newProfileId)) {
             _setTokenURI(newProfileId, _uri);
+
+            _setUsername(profile, _username);
             return newProfileId;
         }
         _mint(profile, newProfileId);
         _setTokenURI(newProfileId, _uri);
 
+        _setUsername(profile, _username);
         return newProfileId;
     }
 
     function get(address profile) public view returns (string memory) {
+        uint256 profileId = _fromAddressToId(profile);
+        return tokenURI(profileId);
+    }
+
+    function getFromUsername(
+        bytes32 username
+    ) public view returns (string memory) {
+        address profile = profiles[username];
+        require(profile != address(0), "This username does not exist.");
+
         uint256 profileId = _fromAddressToId(profile);
         return tokenURI(profileId);
     }
@@ -66,7 +101,28 @@ contract Profile is
     function _burn(
         uint256 tokenId
     ) internal override(ERC721URIStorageUpgradeable) {
+        _unsetUsername(_fromIdToAddress(tokenId));
         super._burn(tokenId);
+    }
+
+    function _setUsername(address profile, bytes32 username) internal {
+        if (usernames[profile] != NULL) {
+            delete profiles[usernames[profile]];
+        }
+
+        if (profiles[username] != address(0)) {
+            delete usernames[profiles[username]];
+        }
+
+        profiles[username] = profile;
+        usernames[profile] = username;
+    }
+
+    function _unsetUsername(address _profile) internal {
+        bytes32 username = usernames[_profile];
+
+        delete profiles[username];
+        delete usernames[_profile];
     }
 
     function _fromIdToAddress(uint256 tokenId) internal pure returns (address) {
--- test/Profile.ts
+++ test/Profile.ts
@@ -2,6 +2,10 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
 
+const friend1UsernameA = ethers.utils.formatBytes32String("friend1a");
+const friend1UsernameB = ethers.utils.formatBytes32String("friend1b");
+const friend2Username = ethers.utils.formatBytes32String("friend2");
+
 // sould bound ERC721 with profile metadata uri
 describe("Profile", function () {
   async function deployProfileFixture() {
@@ -29,16 +33,29 @@ describe("Profile", function () {
       const { profile, owner, friend1, friend2 } = await loadFixture(
         deployProfileFixture
       );
-      await profile.connect(friend1).set(friend1.address, "https://test.com");
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
       expect(await profile.get(friend1.address)).to.equal("https://test.com");
+      expect(await profile.getFromUsername(friend1UsernameA)).to.equal(
+        "https://test.com"
+      );
 
-      await profile.connect(friend2).set(friend2.address, "https://test2.com");
+      await profile
+        .connect(friend2)
+        .set(friend2.address, friend2Username, "https://test2.com");
       expect(await profile.get(friend2.address)).to.equal("https://test2.com");
+      expect(await profile.getFromUsername(friend2Username)).to.equal(
+        "https://test2.com"
+      );
 
       await profile
         .connect(friend1)
-        .set(friend1.address, "https://testing.com");
+        .set(friend1.address, friend1UsernameA, "https://testing.com");
       expect(await profile.get(friend1.address)).to.equal(
+        "https://testing.com"
+      );
+      expect(await profile.getFromUsername(friend1UsernameA)).to.equal(
         "https://testing.com"
       );
     });
@@ -48,11 +65,15 @@ describe("Profile", function () {
         deployProfileFixture
       );
       await expect(
-        profile.connect(friend2).set(friend1.address, "https://test.com")
+        profile
+          .connect(friend2)
+          .set(friend1.address, friend1UsernameA, "https://test.com")
       ).to.be.revertedWith("Only the profile owner can set it.");
 
       await expect(
-        profile.connect(owner).set(friend1.address, "https://test.com")
+        profile
+          .connect(owner)
+          .set(friend1.address, friend1UsernameA, "https://test.com")
       ).to.be.revertedWith("Only the profile owner can set it.");
     });
 
@@ -61,13 +82,17 @@ describe("Profile", function () {
         deployProfileFixture
       );
       expect(await profile.balanceOf(friend1.address)).to.equal(0);
-      await profile.connect(friend1).set(friend1.address, "https://test.com");
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
       expect(await profile.balanceOf(friend1.address)).to.equal(1);
 
       expect(await profile.get(friend1.address)).to.equal("https://test.com");
 
       expect(await profile.balanceOf(friend2.address)).to.equal(0);
-      await profile.connect(friend2).set(friend2.address, "https://test2.com");
+      await profile
+        .connect(friend2)
+        .set(friend2.address, friend2Username, "https://test2.com");
 
       expect(await profile.balanceOf(friend2.address)).to.equal(1);
       expect(await profile.get(friend2.address)).to.equal("https://test2.com");
@@ -77,7 +102,9 @@ describe("Profile", function () {
       const { profile, owner, friend1, friend2 } = await loadFixture(
         deployProfileFixture
       );
-      await profile.connect(friend1).set(friend1.address, "https://test.com");
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
       await expect(
         profile
           .connect(friend1)
@@ -95,7 +122,9 @@ describe("Profile", function () {
       const { profile, owner, friend1, friend2 } = await loadFixture(
         deployProfileFixture
       );
-      await profile.connect(friend1).set(friend1.address, "https://test.com");
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
       await expect(
         profile.connect(friend2).burn(ethers.BigNumber.from(friend1.address))
       ).to.be.revertedWith(
@@ -110,7 +139,9 @@ describe("Profile", function () {
       const { profile, owner, friend1, friend2 } = await loadFixture(
         deployProfileFixture
       );
-      await profile.connect(friend1).set(friend1.address, "https://test.com");
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
       await expect(
         profile.connect(friend2).burn(ethers.BigNumber.from(friend1.address))
       ).to.be.revertedWith(
@@ -128,14 +159,90 @@ describe("Profile", function () {
         deployProfileFixture
       );
       expect(await profile.balanceOf(friend1.address)).to.equal(0);
-      await profile.connect(friend1).set(friend1.address, "https://test.com");
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
       expect(await profile.balanceOf(friend1.address)).to.equal(1);
       await profile
         .connect(friend1)
         .burn(ethers.BigNumber.from(friend1.address));
       expect(await profile.balanceOf(friend1.address)).to.equal(0);
-      await profile.connect(friend1).set(friend1.address, "https://test.com");
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
       expect(await profile.balanceOf(friend1.address)).to.equal(1);
+    });
+
+    it("Should not allow setting a username that belongs to someone else", async function () {
+      const { profile, owner, friend1, friend2 } = await loadFixture(
+        deployProfileFixture
+      );
+
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
+
+      expect(
+        await profile
+          .connect(friend2)
+          .set(friend2.address, friend1UsernameA, "https://test2.com")
+      ).to.be.revertedWith("This username is already taken.");
+    });
+
+    // set a username, then set another one, the value should be the new one
+    it("Should replace the username to the new one when set a second time", async function () {
+      const { profile, owner, friend1, friend2 } = await loadFixture(
+        deployProfileFixture
+      );
+
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
+
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameB, "https://test.com");
+
+      expect(await profile.getFromUsername(friend1UsernameB)).to.equal(
+        "https://test.com"
+      );
+
+      await expect(
+        profile.getFromUsername(friend1UsernameA)
+      ).to.be.revertedWith("This username does not exist.");
+    });
+
+    // set a username, then set another one, the old username should be available again
+    it("Should replace the username to the new one when set a second time", async function () {
+      const { profile, owner, friend1, friend2 } = await loadFixture(
+        deployProfileFixture
+      );
+
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameA, "https://test.com");
+
+      expect(
+        await profile
+          .connect(friend2)
+          .set(friend2.address, friend1UsernameA, "https://test2.com")
+      ).to.be.revertedWith("This username is already taken.");
+
+      await profile
+        .connect(friend1)
+        .set(friend1.address, friend1UsernameB, "https://test.com");
+
+      await profile
+        .connect(friend2)
+        .set(friend2.address, friend1UsernameA, "https://test2.com");
+
+      expect(await profile.getFromUsername(friend1UsernameB)).to.equal(
+        "https://test.com"
+      );
+
+      expect(await profile.getFromUsername(friend1UsernameA)).to.equal(
+        "https://test2.com"
+      );
     });
   });
 });
