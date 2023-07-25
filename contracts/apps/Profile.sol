98e28b5d223b0fd2474162ad940570a97e2a2f18
Alexandre Collette <alexcollette@hotmail.com>
2023-07-25 15:52:38 +0200

    optimized set()

--- contracts/apps/Profile.sol
+++ contracts/apps/Profile.sol
@@ -42,11 +42,11 @@ contract Profile is
     ) public returns (uint256) {
         require(profile == msg.sender, "Only the profile owner can set it.");
 
-        bytes32 username = usernames[profile];
+        bytes32 currentUsername = usernames[profile];
 
         require(
-            (username == NULL) || // username is not set
-                (username != NULL && profiles[username] == profile), // username is set but belongs to the profile
+            (currentUsername == NULL) || // username is not set
+                (currentUsername != NULL && profiles[currentUsername] == profile), // username is set but belongs to the profile
             "This username is already taken."
         );
 
@@ -54,9 +54,13 @@ contract Profile is
         if (!_exists(newProfileId)) {
             _mint(profile, newProfileId);
         }
-        _setTokenURI(newProfileId, _uri);
+        if (currentUsername != _username) {
+            _setUsername(profile, _username);
+        }
+        if (keccak256(abi.encodePacked(tokenURI(newProfileId))) != keccak256(abi.encodePacked(_uri))) {
+            _setTokenURI(newProfileId, _uri);
+        } 
 
-        _setUsername(profile, _username);
         return newProfileId;
     }
 
