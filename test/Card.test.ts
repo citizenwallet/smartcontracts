4b9c4f9ffc83bc5ea06f1b166be801057257531f
kev-techi <kevin@techi.be>
2023-10-12 19:07:38 +0200

    [added] timestamps and whitelist to tests

--- test/Card.test.ts
+++ test/Card.test.ts
@@ -2,6 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
 import { config } from "dotenv";
+import { Contract } from "ethers";
 
 // sould bound ERC721 with profile metadata uri
 describe("Card", function () {
@@ -9,27 +10,7 @@ describe("Card", function () {
   const entrypoint = process.env["ERC4337_ENTRYPOINT"]!;
 
   async function deployCardFactoryFixture() {
-    const [owner, friend1, friend2, deployed1, deployed2] =
-      await ethers.getSigners();
-
-    const CardFactoryContract = await ethers.getContractFactory("CardFactory");
-    const cardFactory = await upgrades.deployProxy(
-      CardFactoryContract,
-      [ethers.utils.getAddress(entrypoint)],
-      {
-        kind: "uups",
-        initializer: "initialize",
-      }
-    );
-
-    await cardFactory.createAccount(
-      deployed1.address,
-      ethers.BigNumber.from(0)
-    );
-    await cardFactory.createAccount(
-      deployed2.address,
-      ethers.BigNumber.from(0)
-    );
+    const [owner, friend1, friend2] = await ethers.getSigners();
 
     const WhitelistContract = await ethers.getContractFactory("Whitelist");
     const whitelist = await upgrades.deployProxy(WhitelistContract, [], {
@@ -37,14 +18,55 @@ describe("Card", function () {
       initializer: "initialize",
     });
 
+    const TimestampsLibraryContract = await ethers.getContractFactory(
+      "Timestamps"
+    );
+    const timestamps = await upgrades.deployProxy(
+      TimestampsLibraryContract,
+      [],
+      {
+        kind: "uups",
+        initializer: "initialize",
+      }
+    );
+
+    const CardFactoryContract = await ethers.getContractFactory("CardFactory");
+    const cardFactory = await upgrades.deployProxy(
+      CardFactoryContract,
+      [
+        ethers.utils.getAddress(entrypoint),
+        whitelist.address,
+        timestamps.address,
+      ],
+      {
+        kind: "uups",
+        initializer: "initialize",
+      }
+    );
+
+    await cardFactory.createAccount(friend1.address, ethers.BigNumber.from(0));
+    await cardFactory.createAccount(friend2.address, ethers.BigNumber.from(0));
+
+    const card1 = await ethers.getContractAt(
+      "Card",
+      await cardFactory.getAddress(friend1.address, ethers.BigNumber.from(0))
+    );
+
+    const card2 = await ethers.getContractAt(
+      "Card",
+      await cardFactory.getAddress(friend2.address, ethers.BigNumber.from(0))
+    );
+
     return {
       cardFactory,
       whitelist,
       owner,
       friend1,
       friend2,
-      deployed1,
-      deployed2,
+      // deployed1,
+      // deployed2,
+      card1,
+      card2,
     };
   }
 
@@ -70,37 +92,34 @@ describe("Card", function () {
 
   describe("Card Deployment", function () {
     it("Should be owned by the caller", async function () {
-      const { cardFactory, deployed1, deployed2 } = await loadFixture(
+      const { cardFactory, friend1 } = await loadFixture(
         deployCardFactoryFixture
       );
 
       const card = await ethers.getContractAt(
         "Card",
-        await cardFactory.getAddress(
-          deployed1.address,
-          ethers.BigNumber.from(0)
-        )
+        await cardFactory.getAddress(friend1.address, ethers.BigNumber.from(0))
       );
 
-      expect(await card.owner()).to.equal(deployed1.address);
+      expect(await card.owner()).to.equal(friend1.address);
     });
 
     it("Should be possible to change the owner", async function () {
-      const { cardFactory, deployed1, deployed2 } = await loadFixture(
+      const { cardFactory, friend1, friend2 } = await loadFixture(
         deployCardFactoryFixture
       );
 
-      const address = deployed1.address;
-      const address2 = deployed2.address;
+      const address = friend1.address;
+      const address2 = friend2.address;
 
       const card = await ethers.getContractAt(
         "Card",
         await cardFactory.getAddress(address, ethers.BigNumber.from(0))
       );
 
-      await card.connect(deployed1).transferOwnership(address2);
+      await card.connect(friend1).transferOwnership(address2);
 
-      expect(await card.connect(deployed1).owner()).to.equal(address2);
+      expect(await card.connect(friend2).owner()).to.equal(address2);
     });
   });
 
@@ -154,11 +173,23 @@ describe("Card", function () {
     });
   });
 
-  describe("Account", function () {
-    it("Should return the same address twice", async function () {
-      const { cardFactory, owner, friend1, friend2 } = await loadFixture(
+  describe("Card", function () {
+    it("Should not be expired", async function () {
+      const { card1, friend1, card2, friend2 } = await loadFixture(
         deployCardFactoryFixture
       );
+
+      expect(await card1.connect(friend1).hasExpired()).to.equal(false);
+      expect(await card2.connect(friend2).hasExpired()).to.equal(false);
+    });
+
+    it("Should allow anyone to check the expiry", async function () {
+      const { card1, friend1, card2, friend2 } = await loadFixture(
+        deployCardFactoryFixture
+      );
+
+      expect(await card1.connect(friend2).hasExpired()).to.equal(false);
+      expect(await card2.connect(friend1).hasExpired()).to.equal(false);
     });
   });
 });
