a9226a93ca4bae7aaf8e28dd9cae3f83256def68
kev-techi <kevin@techi.be>
2023-11-02 13:31:00 +0100

    [added] test case for transferring via authorizer

--- test/Account.test.ts
+++ test/Account.test.ts
@@ -2,16 +2,167 @@ import fs from "fs";
 import path from "path";
 import "@nomicfoundation/hardhat-toolbox";
 import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers, upgrades, network } from "hardhat";
 import { config } from "dotenv";
-import { providers, ContractFactory } from "ethers";
-import { UserOperationBuilder, Presets } from "userop";
-import { keccak256 } from "ethers/lib/utils";
+import { ContractFactory, BigNumber, BigNumberish, BytesLike } from "ethers";
+
+interface IUserOp {
+  sender: string;
+  nonce: BigNumberish;
+  initCode: BytesLike;
+  callData: BytesLike;
+  callGasLimit: BigNumberish;
+  verificationGasLimit: BigNumberish;
+  preVerificationGas: BigNumberish;
+  maxFeePerGas: BigNumberish;
+  maxPriorityFeePerGas: BigNumberish;
+  paymasterAndData: BytesLike;
+  signature?: BytesLike;
+}
+
+interface UserOpCreation {
+  sender: string;
+  receiver: string;
+  amount: BigNumberish;
+  tokenAddress: string;
+  signerAddress: string;
+  accountFactoryAddress: string;
+  authorizerContract: any; // using Contract type causes errors
+  authorizer: SignerWithAddress;
+}
+
+const createUserOp = async ({
+  sender,
+  receiver,
+  amount,
+  tokenAddress,
+  signerAddress,
+  accountFactoryAddress,
+  authorizerContract,
+  authorizer,
+}: UserOpCreation) => {
+  const userop: IUserOp = {
+    sender,
+    nonce: ethers.BigNumber.from("0"),
+    initCode: ethers.utils.arrayify("0x"),
+    callData: ethers.utils.arrayify("0x"),
+    callGasLimit: ethers.BigNumber.from("0"),
+    verificationGasLimit: ethers.BigNumber.from("0"),
+    preVerificationGas: ethers.BigNumber.from("0"),
+    maxFeePerGas: ethers.BigNumber.from("0"),
+    maxPriorityFeePerGas: ethers.BigNumber.from("0"),
+    paymasterAndData: ethers.utils.arrayify("0x"),
+    signature: ethers.utils.arrayify("0x"),
+  };
+
+  // nonce
+  const nonce: BigNumber = await authorizerContract.getNonce(sender);
+
+  userop.nonce = nonce;
+
+  // initCode
+  if (userop.nonce.eq(ethers.constants.Zero)) {
+    const accountCreationCode = accountCreate.encodeFunctionData(
+      "createAccount",
+      [signerAddress, ethers.BigNumber.from(0)]
+    );
+
+    userop.initCode = ethers.utils.hexConcat([
+      accountFactoryAddress,
+      accountCreationCode,
+    ]);
+  }
+
+  // callData
+  userop.callData = accountExecute.encodeFunctionData("execute", [
+    tokenAddress,
+    ethers.constants.Zero,
+    erc20Token.encodeFunctionData("transfer", [receiver, amount]),
+  ]);
+
+  // paymasterAndData
+
+  const current = await time.latest();
+
+  const hash = ethers.utils.arrayify(
+    await authorizerContract.getHash(userop, current + 86400, current)
+  );
+
+  const signedHash = await authorizer.signMessage(hash);
+
+  // Define the types of the values
+  const types = ["uint48", "uint48"];
+
+  // Define the values
+  const values = [current + 86400, current];
+
+  // ABI encode the values
+  const encoded = ethers.utils.defaultAbiCoder.encode(types, values);
+
+  userop.paymasterAndData = ethers.utils.hexConcat([
+    authorizerContract.address,
+    encoded,
+    signedHash,
+  ]);
+
+  return userop;
+};
+
+const getUserOpHash = (userop: IUserOp, authorizerContract: any) => {
+  const packedData = ethers.utils.defaultAbiCoder.encode(
+    [
+      "address",
+      "uint256",
+      "bytes32",
+      "bytes32",
+      "uint256",
+      "uint256",
+      "uint256",
+      "uint256",
+      "uint256",
+      "bytes32",
+    ],
+    [
+      userop.sender,
+      userop.nonce,
+      ethers.utils.keccak256(userop.initCode),
+      ethers.utils.keccak256(userop.callData),
+      userop.callGasLimit,
+      userop.verificationGasLimit,
+      userop.preVerificationGas,
+      userop.maxFeePerGas,
+      userop.maxPriorityFeePerGas,
+      ethers.utils.keccak256(userop.paymasterAndData),
+    ]
+  );
+
+  const enc = ethers.utils.defaultAbiCoder.encode(
+    ["bytes32", "address", "uint256"],
+    [
+      ethers.utils.keccak256(packedData),
+      authorizerContract.address,
+      network.config.chainId,
+    ]
+  );
+  return ethers.utils.keccak256(enc);
+};
+
+const signUserOp = async (
+  userop: IUserOp,
+  authorizerContract: any,
+  signer: SignerWithAddress
+) => {
+  const userOpHash = getUserOpHash(userop, authorizerContract);
+
+  return await signer.signMessage(ethers.utils.arrayify(userOpHash));
+};
 
 const accountExecuteABI = [
   "function execute(address to, uint256 value, bytes data)",
 ];
+const accountCreateABI = ["function createAccount(address, uint256)"];
 const accountUpdateAuthorizerABI = [
   "function updateAuthorizer(address newAuthorizer)",
 ];
@@ -26,6 +177,7 @@ const partialERC20TokenABI = [
 ];
 
 const accountExecute = new ethers.utils.Interface(accountExecuteABI);
+const accountCreate = new ethers.utils.Interface(accountCreateABI);
 const accountUpdateAuthorizer = new ethers.utils.Interface(
   accountUpdateAuthorizerABI
 );
@@ -66,6 +218,19 @@ describe("Account", function () {
     );
     const entrypoint = await EntryPointContract.deploy();
 
+    const AuthorizerContract = await ethers.getContractFactory("Authorizer", {
+      signer: owner,
+    });
+
+    const authorizerContract = await upgrades.deployProxy(
+      AuthorizerContract,
+      [authorizer.address],
+      {
+        kind: "uups",
+        initializer: "initialize",
+      }
+    );
+
     const AccountFactoryContract = await ethers.getContractFactory(
       "AccountFactory",
       {
@@ -75,7 +240,7 @@ describe("Account", function () {
 
     const accountFactory = await AccountFactoryContract.deploy(
       entrypoint.address,
-      authorizer.address
+      authorizerContract.address
     );
 
     await accountFactory.createAccount(
@@ -93,30 +258,10 @@ describe("Account", function () {
       await accountFactory.getAddress(friend1.address, ethers.BigNumber.from(0))
     );
 
-    await account1
-      .connect(authorizer)
-      .execute(
-        account1.address,
-        ethers.constants.Zero,
-        accountUpdateWhitelist.encodeFunctionData("updateWhitelist", [
-          [token.address],
-        ])
-      );
-
     const account2 = await ethers.getContractAt(
       "Account",
       await accountFactory.getAddress(friend2.address, ethers.BigNumber.from(0))
     );
-
-    await account2
-      .connect(authorizer)
-      .execute(
-        account2.address,
-        ethers.constants.Zero,
-        accountUpdateWhitelist.encodeFunctionData("updateWhitelist", [
-          [token.address],
-        ])
-      );
 
     await entrypoint.connect(owner).depositTo(owner.address, {
       value: ethers.BigNumber.from("1000000000000000000"),
@@ -129,13 +274,10 @@ describe("Account", function () {
       value: ethers.BigNumber.from("1000000000000000000"),
     });
 
-    // add funds
-    // await token.connect(owner).mint(account1.address, 1000000000, "owner");
-    // await token.connect(owner).mint(friend2.address, 1000000000, "friend1");
-
     return {
       entrypoint,
       token,
+      authorizerContract,
       accountFactory,
       owner,
       friend1,
@@ -200,25 +342,18 @@ describe("Account", function () {
       expect(await token.balanceOf(account2.address)).to.equal(0);
     });
 
-    it("Authorizer should not be able to transfer ERC20 directly unless whitelisted (authorizer)", async function () {
-      const { owner, token, friend2, friend3, authorizer, accountFactory } =
-        await loadFixture(deployCardFactoryFixture);
+    it("Authorizer should be able to transfer ERC20 if user op signed by user (authorizer)", async function () {
+      const {
+        owner,
+        token,
+        authorizerContract,
+        friend2,
+        friend3,
+        authorizer,
+        accountFactory,
+      } = await loadFixture(deployCardFactoryFixture);
 
       const address = await accountFactory.getAddress(
-        friend3.address,
-        ethers.BigNumber.from(0)
-      );
-
-      await accountFactory.createAccount(
-        friend3.address,
-        ethers.BigNumber.from(0)
-      );
-
-      const account = await ethers.getContractAt("Account", address);
-
-      await account.connect(friend3).updateAuthorizer(authorizer.address);
-
-      await accountFactory.createAccount(
         friend3.address,
         ethers.BigNumber.from(0)
       );
@@ -230,100 +365,37 @@ describe("Account", function () {
 
       const mintedAmount = 1000000000;
 
-      await token.connect(owner).mint(account.address, mintedAmount, "owner");
+      await token.connect(owner).mint(address, mintedAmount, "owner");
 
       // balance should match what was minted
-      expect(await token.balanceOf(account.address)).to.equal(mintedAmount);
+      expect(await token.balanceOf(address)).to.equal(mintedAmount);
 
-      // transfer to friend2
-      await expect(
-        account
-          .connect(authorizer)
-          .execute(
-            token.address,
-            ethers.constants.Zero,
-            erc20Token.encodeFunctionData("transfer", [accountAddress2, 100n])
-          )
-      ).to.be.revertedWith("account: address not whitelisted");
+      const transferAmount = 100n;
 
-      await account.connect(friend3).updateWhitelist([token.address]);
+      const userop = await createUserOp({
+        sender: address,
+        receiver: accountAddress2,
+        amount: transferAmount,
+        tokenAddress: token.address,
+        signerAddress: friend3.address,
+        accountFactoryAddress: accountFactory.address,
+        authorizerContract,
+        authorizer,
+      });
 
-      // transfer to friend2
-      await account
-        .connect(authorizer)
-        .execute(
-          token.address,
-          ethers.constants.Zero,
-          erc20Token.encodeFunctionData("transfer", [accountAddress2, 100n])
-        );
+      userop.signature = await signUserOp(userop, authorizerContract, friend3);
+
+      await authorizerContract.handleOps([userop], authorizer.address);
 
       // balance should match what was sent
-      expect(await token.balanceOf(accountAddress2)).to.equal(100n);
+      expect(await token.balanceOf(accountAddress2)).to.equal(transferAmount);
+
+      // cannot replay transaction
+      await expect(
+        authorizerContract.handleOps([userop], authorizer.address)
+      ).to.be.revertedWith("invalid nonce");
     });
   });
-
-  // describe("Nonce", function () {
-  //   it("Authorizer execution should increment nonce", async function () {
-  //     const { owner, token, friend2, friend3, authorizer, accountFactory } =
-  //       await loadFixture(deployCardFactoryFixture);
-
-  //     const address = await accountFactory.getAddress(
-  //       friend3.address,
-  //       ethers.BigNumber.from(0)
-  //     );
-
-  //     await accountFactory.createAccount(
-  //       friend3.address,
-  //       ethers.BigNumber.from(0)
-  //     );
-
-  //     const account = await ethers.getContractAt("Account", address);
-
-  //     await account.connect(friend3).updateAuthorizer(authorizer.address);
-
-  //     await accountFactory.createAccount(
-  //       friend3.address,
-  //       ethers.BigNumber.from(0)
-  //     );
-
-  //     const accountAddress2 = await accountFactory.getAddress(
-  //       friend2.address,
-  //       ethers.BigNumber.from(0)
-  //     );
-
-  //     const mintedAmount = 1000000000;
-
-  //     await token.connect(owner).mint(account.address, mintedAmount, "owner");
-
-  //     // balance should match what was minted
-  //     expect(await token.balanceOf(account.address)).to.equal(mintedAmount);
-
-  //     // transfer to friend2
-  //     await expect(
-  //       account
-  //         .connect(authorizer)
-  //         .execute(
-  //           token.address,
-  //           ethers.constants.Zero,
-  //           erc20Token.encodeFunctionData("transfer", [accountAddress2, 100n])
-  //         )
-  //     ).to.be.revertedWith("account: address not whitelisted");
-
-  //     await account.connect(friend3).updateWhitelist([token.address]);
-
-  //     // transfer to friend2
-  //     await account
-  //       .connect(authorizer)
-  //       .execute(
-  //         token.address,
-  //         ethers.constants.Zero,
-  //         erc20Token.encodeFunctionData("transfer", [accountAddress2, 100n])
-  //       );
-
-  //     // balance should match what was sent
-  //     expect(await token.balanceOf(accountAddress2)).to.equal(100n);
-  //   });
-  // });
 
   describe("Factory", function () {
     it("Should be owned by the caller", async function () {
