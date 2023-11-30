97e90e93176cfe01d068dfd97d71f923741dc946
Xavier Damman <xdamman@gmail.com>
2023-11-30 08:06:18 +0100

    keeping env variables consistent across networks.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,15 +17,29 @@ const hhconfig: HardhatUserConfig = {
     },
   },
   networks: {
-    mainnet: {
+    ethereum: {
       url: process.env.ETHEREUM_RPC_URL,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
-      gasPrice: 9000000000, // this is 30 Gwei
+      gasPrice: 9000000000, // this is 90 Gwei
     },
     polygon: {
       url: process.env.POLYGON_RPC_URL,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
-      gasPrice: 90000000000, // this is 30 Gwei
+      gasPrice: 90000000000, // this is 90 Gwei
+      timeout: 999999,
+      // timeoutBlocks: 200,
+      gas: 12400000,
+      // gasPrice: 900000000000,
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: true,
+      allowUnlimitedContractSize: true,
+      // blockGasLimit: 0x1fffffffffffff,
+      // timeout: 1800000,
+    },
+    polygon_testnet: {
+      url: process.env.POLYGON_TESTNET_RPC_URL,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
+      gasPrice: 90000000000, // this is 90 Gwei
       timeout: 999999,
       // timeoutBlocks: 200,
       gas: 12400000,
@@ -41,38 +55,21 @@ const hhconfig: HardhatUserConfig = {
       accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
       gasPrice: 30000000, // this is 30 Gwei
     },
+    base_testnet: {
+      url: process.env.BASE_TESTNET_RPC_URL,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
+      gasPrice: 30000000, // this is 30 Gwei
+    },
     celo: {
       url: process.env.CELO_RPC_URL,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
       gasPrice: 5000000000, // this is 30 Gwei
     },
-  },
-  etherscan: {
-    apiKey: {
-      mainnet: process.env.ETEHREUM_MAINNET_ETHERSCAN_API_KEY || "",
-      polygonMumbai: process.env.POLYGON_MUMBAI_ETHERSCAN_API_KEY || "",
-      polygon: process.env.POLYGON_MAINNET_ETHERSCAN_API_KEY || "",
-      base: process.env.BASE_MAINNET_ETHERSCAN_API_KEY || "",
-      celo: process.env.CELO_MAINNET_ETHERSCAN_API_KEY || "",
+    celo_testnet: {
+      url: process.env.CELO_TESTNET_RPC_URL,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
+      gasPrice: 5000000000, // this is 30 Gwei
     },
-    customChains: [
-      {
-        network: "base",
-        chainId: 8453,
-        urls: {
-          apiURL: "https://api.basescan.org/api",
-          browserURL: "https://basescan.org",
-        },
-      },
-      {
-        network: "celo",
-        chainId: 42220,
-        urls: {
-          apiURL: "https://api.celoscan.io/api",
-          browserURL: "https://celoscan.io",
-        },
-      },
-    ],
   },
 };
 
