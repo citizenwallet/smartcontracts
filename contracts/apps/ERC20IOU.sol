69f39c79de216ec48dcbc590db8c2f64b069de3a
Kevin Sundar Raj <122159642+kev-techi@users.noreply.github.com>
2024-02-21 12:59:19 +0100

    Token IOU Contract (#13)
    * added token iou contract
    
    * added test case for redeeming on another contract
    
    * updated naming of variables and errors
    
    * updated naming of variables and errors
    
    * updated naming of variables and errors
    
    * updated naming of variables and errors
    
    * add deploy function for iou

---
+++ contracts/apps/ERC20IOU.sol
@@ -0,0 +1,212 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+
+import "@openzeppelin/contracts/interfaces/IERC1271.sol";
+
+contract ERC20IOU is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    IERC20Upgradeable public token;
+
+    /// only a single redeem per hash
+    ///
+    /// allows us to check which username was already reserved by a profile
+    mapping(bytes32 redeemHash => uint48 time) public redeemed;
+
+    // Redeem event
+    event Redeem(address indexed from, address indexed to, uint256 amount);
+
+    function initialize(IERC20Upgradeable _token) public initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+        token = _token;
+    }
+
+    /**
+    * @notice Get the hash of an IOU
+    * @param from       The address of the issuer
+    * @param amount     The uint256 amount of tokens to redeem
+    * @param validUntil The uint48 timestamp until which the redeem is valid
+    * @param validAfter The uint48 timestamp from which the redeem is valid
+    * @param sequence   The uint256 unique number to prevent replay attacks
+    * @return bytes32   The hash of an IOU
+
+    Can be used to easily generate a valid IOU signature for the redeem function
+*/
+    function getHash(
+        address from,
+        uint256 amount,
+        uint48 validUntil,
+        uint48 validAfter,
+        uint256 sequence
+    ) public view returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked(
+                    from,
+                    amount,
+                    validUntil,
+                    validAfter,
+                    sequence,
+                    block.chainid,
+                    address(this)
+                )
+            );
+    }
+
+    /**
+     * @notice Redeem tokens
+     * @param from       The address of the issuer
+     * @param amount     The uint256 amount of tokens to redeem
+     * @param validUntil The uint48 timestamp until which the redeem is valid
+     * @param validAfter The uint48 timestamp from which the redeem is valid
+     * @param sequence   The uint256 unique number to prevent replay attacks
+     * @param signature  The bytes calldata signature of the IOU
+     */
+    function redeem(
+        address from,
+        uint256 amount,
+        uint48 validUntil,
+        uint48 validAfter,
+        uint256 sequence,
+        bytes calldata signature
+    ) public {
+        uint48 currentTime = uint48(block.timestamp);
+        require(
+            currentTime >= validAfter && currentTime < validUntil,
+            "ERC20IOU: expired or not valid yet"
+        );
+
+        bytes32 redeemHash = keccak256(
+            abi.encodePacked(
+                "\x19Ethereum Signed Message:\n32",
+                getHash(from, amount, validUntil, validAfter, sequence)
+            )
+        );
+
+        require(redeemed[redeemHash] == 0, "ERC20IOU: already redeemed");
+
+        // check the sender's signature
+
+        // check if the msg.sender is a smart contract
+        if (_contractExists(msg.sender)) {
+            // check ownership of the contract
+            IERC1271 account = IERC1271(msg.sender);
+
+            require(
+                account.isValidSignature(redeemHash, signature) == 0x1626ba7e,
+                "ERC20IOU: invalid signature"
+            );
+        } else {
+            // check the signature of the sender
+            require(
+                _recoverSigner(redeemHash, signature) == msg.sender,
+                "ERC20IOU: invalid signature"
+            );
+        }
+
+        token.transferFrom(from, msg.sender, amount);
+
+        redeemed[redeemHash] = currentTime;
+
+        emit Redeem(from, msg.sender, amount);
+    }
+
+    // ************************
+
+    // Recovering signatures
+
+    function _readBytes32(
+        bytes memory data,
+        uint256 index
+    ) internal pure returns (bytes32 result) {
+        require(data.length >= index + 32, "readBytes32: invalid data length");
+        assembly {
+            result := mload(add(data, add(32, index)))
+        }
+    }
+
+    /**
+     * @notice Recover the signer of hash, assuming it's an EOA account
+     * @dev Only for EthSign signatures
+     * @param _hash       Hash of message that was signed
+     * @param _signature  Signature encoded as (bytes32 r, bytes32 s, uint8 v)
+     */
+    function _recoverSigner(
+        bytes32 _hash,
+        bytes memory _signature
+    ) internal pure returns (address signer) {
+        require(
+            _signature.length == 65,
+            "SignatureValidator#recoverSigner: invalid signature length"
+        );
+
+        // Variables are not scoped in Solidity.
+        uint8 v = uint8(_signature[64]);
+        bytes32 r = _readBytes32(_signature, 0);
+        bytes32 s = _readBytes32(_signature, 32);
+
+        // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
+        // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
+        // the valid range for s in (281): 0 < s < secp256k1n ÷ 2 + 1, and for v in (282): v ∈ {27, 28}. Most
+        // signatures from current libraries generate a unique signature with an s-value in the lower half order.
+        //
+        // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
+        // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
+        // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
+        // these malleable signatures as well.
+        //
+        // Source OpenZeppelin
+        // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/cryptography/ECDSA.sol
+
+        if (
+            uint256(s) >
+            0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0
+        ) {
+            revert(
+                "SignatureValidator#recoverSigner: invalid signature 's' value"
+            );
+        }
+
+        if (v != 27 && v != 28) {
+            revert(
+                "SignatureValidator#recoverSigner: invalid signature 'v' value"
+            );
+        }
+
+        // Recover ECDSA signer
+        signer = ecrecover(_hash, v, r, s);
+
+        // Prevent signer from being 0x0
+        require(
+            signer != address(0x0),
+            "SignatureValidator#recoverSigner: INVALID_SIGNER"
+        );
+
+        return signer;
+    }
+
+    // ************************
+
+    /**
+     * @dev Checks if a contract exists at a given address.
+     * @param contractAddress The address to check.
+     * @return A boolean indicating whether a contract exists at the given address.
+     */
+    function _contractExists(
+        address contractAddress
+    ) internal virtual returns (bool) {
+        uint size;
+        assembly {
+            size := extcodesize(contractAddress)
+        }
+        return size > 0;
+    }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
+}
---
+++ test/ERC20IOU.test.ts
@@ -0,0 +1,269 @@
+import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { ethers, upgrades } from "hardhat";
+
+const getHash = (
+  from: string,
+  amount: number,
+  validUntil: number,
+  validFrom: number,
+  sequence: number,
+  chainId: number,
+  contract: string
+) => {
+  return ethers.utils.solidityKeccak256(
+    ["address", "uint256", "uint48", "uint48", "uint256", "uint256", "address"],
+    [from, amount, validUntil, validFrom, sequence, chainId, contract]
+  );
+};
+
+// sould bound ERC721 with tokeniou metadata uri
+describe("ERC20IOU", function () {
+  async function deployTokenIOUFixture() {
+    const [owner, friend1, friend2] = await ethers.getSigners();
+
+    const TokenContract = await ethers.getContractFactory(
+      "UpgradeableBurnableCommunityToken",
+      {
+        signer: owner,
+      }
+    );
+    const token = await upgrades.deployProxy(
+      TokenContract,
+      [[owner.address], "My Token", "MT"],
+      {
+        kind: "uups",
+        initializer: "initialize",
+      }
+    );
+
+    const TokenIOUContract = await ethers.getContractFactory("ERC20IOU");
+    const tokeniou = await upgrades.deployProxy(
+      TokenIOUContract,
+      [token.address],
+      {
+        kind: "uups",
+        initializer: "initialize",
+      }
+    );
+
+    const tokeniou2 = await upgrades.deployProxy(
+      TokenIOUContract,
+      [token.address],
+      {
+        kind: "uups",
+        initializer: "initialize",
+      }
+    );
+
+    const network = await ethers.provider.getNetwork();
+
+    const current = await time.latest();
+
+    return {
+      network,
+      current,
+      tokeniou,
+      tokeniou2,
+      token,
+      owner,
+      friend1,
+      friend2,
+    };
+  }
+
+  async function redeemFixture() {
+    const { current, tokeniou, tokeniou2, token, friend1, friend2 } =
+      await loadFixture(deployTokenIOUFixture);
+
+    const validUntil = current + 2592000; // 30 days
+    const validAfter = current;
+
+    const hash = await tokeniou.getHash(
+      friend2.address,
+      1,
+      validUntil,
+      validAfter,
+      0
+    );
+
+    await token.mint(friend2.address, 1, "hello");
+
+    await token.connect(friend2).approve(tokeniou.address, 1);
+
+    const signature = await friend1.signMessage(ethers.utils.arrayify(hash));
+
+    return {
+      tokeniou,
+      tokeniou2,
+      token,
+      friend1,
+      friend2,
+      current,
+      validUntil,
+      validAfter,
+      signature,
+    };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      const { tokeniou, owner } = await loadFixture(deployTokenIOUFixture);
+
+      expect(await tokeniou.owner()).to.equal(owner.address);
+    });
+  });
+
+  describe("Hash", function () {
+    it("Should generate the right hash", async function () {
+      const { network, current, tokeniou, friend1 } = await loadFixture(
+        deployTokenIOUFixture
+      );
+
+      const validUntil = current + 2592000; // 30 days
+      const validAfter = current;
+
+      const hash = await tokeniou.getHash(
+        friend1.address,
+        1,
+        validUntil,
+        validAfter,
+        0
+      );
+
+      const localHash = getHash(
+        friend1.address,
+        1,
+        validUntil,
+        validAfter,
+        0,
+        network.chainId,
+        tokeniou.address
+      );
+
+      expect(hash).to.equal(localHash);
+    });
+  });
+
+  describe("Redeem", async function () {
+    it("Should allow redeeming a valid IOU", async function () {
+      const {
+        tokeniou,
+        friend2,
+        token,
+        friend1,
+        validUntil,
+        validAfter,
+        signature,
+      } = await loadFixture(redeemFixture);
+
+      expect(await token.balanceOf(friend1.address)).to.equal(0);
+
+      await tokeniou
+        .connect(friend1)
+        .redeem(friend2.address, 1, validUntil, validAfter, 0, signature);
+
+      expect(await token.balanceOf(friend1.address)).to.equal(1);
+    });
+
+    it("Should not allow redeeming again", async function () {
+      const { tokeniou, friend1, friend2, validUntil, validAfter, signature } =
+        await loadFixture(redeemFixture);
+
+      await tokeniou
+        .connect(friend1)
+        .redeem(friend2.address, 1, validUntil, validAfter, 0, signature);
+
+      await expect(
+        tokeniou
+          .connect(friend1)
+          .redeem(friend2.address, 1, validUntil, validAfter, 0, signature)
+      ).to.be.revertedWith("ERC20IOU: already redeemed");
+    });
+
+    it("Should not allow someone to redeem with someone else's signature", async function () {
+      const { tokeniou, friend2, validUntil, validAfter, signature } =
+        await loadFixture(redeemFixture);
+
+      await expect(
+        tokeniou
+          .connect(friend2)
+          .redeem(friend2.address, 1, validUntil, validAfter, 0, signature)
+      ).to.be.revertedWith("ERC20IOU: invalid signature");
+    });
+
+    it("Should not allow tampering with validity", async function () {
+      const { tokeniou, friend1, friend2, current, validUntil, signature } =
+        await loadFixture(redeemFixture);
+
+      const validAfter = current - 2;
+
+      await expect(
+        tokeniou
+          .connect(friend1)
+          .redeem(friend2.address, 1, validUntil, validAfter, 0, signature)
+      ).to.be.revertedWith("ERC20IOU: invalid signature");
+    });
+
+    it("Should not allow redeeming before validity", async function () {
+      const { current, tokeniou, friend1, friend2 } = await loadFixture(
+        deployTokenIOUFixture
+      );
+
+      const validUntil = current + 2592000; // 30 days
+      const validAfter = current + 100;
+
+      const hash = await tokeniou.getHash(
+        friend1.address,
+        1,
+        validUntil,
+        validAfter,
+        0
+      );
+
+      const signature = await friend1.signMessage(ethers.utils.arrayify(hash));
+
+      await expect(
+        tokeniou
+          .connect(friend1)
+          .redeem(friend2.address, 1, validUntil, validAfter, 0, signature)
+      ).to.be.revertedWith("ERC20IOU: expired or not valid yet");
+    });
+
+    it("Should not allow redeeming after validity", async function () {
+      const { current, tokeniou, friend1, friend2 } = await loadFixture(
+        deployTokenIOUFixture
+      );
+
+      const validUntil = current - 2; // 30 days
+      const validAfter = current - 100;
+
+      const hash = await tokeniou.getHash(
+        friend1.address,
+        1,
+        validUntil,
+        validAfter,
+        0
+      );
+
+      const signature = await friend1.signMessage(ethers.utils.arrayify(hash));
+
+      await expect(
+        tokeniou
+          .connect(friend1)
+          .redeem(friend2.address, 1, validUntil, validAfter, 0, signature)
+      ).to.be.revertedWith("ERC20IOU: expired or not valid yet");
+    });
+
+    it("Should not allow redeeming on another contract", async function () {
+      const { tokeniou2, friend1, friend2, validUntil, validAfter, signature } =
+        await loadFixture(redeemFixture);
+
+      await expect(
+        tokeniou2
+          .connect(friend1)
+          .redeem(friend2.address, 1, validUntil, validAfter, 0, signature)
+      ).to.be.revertedWith("ERC20IOU: invalid signature");
+    });
+  });
+});
